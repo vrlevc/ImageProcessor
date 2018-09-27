4f3b8c4cdd982aea8bba2c26779c568848125e80
glvlevch <viktor.levchenko@avid.com>
2018-09-27 16:51:03 +0300

    Initial Test framework for playing with Image I/O framework.

---
+++ ImageIO_Tests/ImageIO_Tests.swift
@@ -0,0 +1,28 @@
+//
+//  ImageIO_Tests.swift
+//  ImageIO_Tests
+//
+//  Created by Viktor Levchenko on 9/27/18.
+//  Copyright © 2018 LVA. All rights reserved.
+//
+
+import XCTest
+
+class ImageIO_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+	
+}
